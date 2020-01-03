a0302dac3f94959f5d42158933b28e8418f3da5f
françois <f_massart@hotmail.com>
2020-01-03 15:45:42 +0100

    Updates

---
+++ precache-manifest.05aaa36fe9fec98dab8b984d1c4e04db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bb3bed0a81f1f8a55ce8f8f71bfd1c",
+    "url": "/portfoliov2/index.html"
+  },
+  {
+    "revision": "d0806516baf37710155b",
+    "url": "/portfoliov2/static/css/main.68e48977.chunk.css"
+  },
+  {
+    "revision": "e1a8f8c297364a12fd3d",
+    "url": "/portfoliov2/static/js/2.289f250c.chunk.js"
+  },
+  {
+    "revision": "5586a95853ee43deda1fb4da37dc3fc8",
+    "url": "/portfoliov2/static/js/2.289f250c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0806516baf37710155b",
+    "url": "/portfoliov2/static/js/main.bca48554.chunk.js"
+  },
+  {
+    "revision": "3883e862bede45489d1f",
+    "url": "/portfoliov2/static/js/runtime-main.5dd117a5.js"
+  }
+]);
